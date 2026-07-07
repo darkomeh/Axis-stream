aaab4c153647955b8882ab0ef4dc0efe6f7a5e4a
darkomeh <realdarkomeh@gmail.com>
2026-07-07 09:11:52 +0100

    refactor: simplify API routing configuration
    Remove redundant URL path reconstruction in the API handler and update Vercel rewrites to point directly to the Express app.

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,3 @@
-import app from "../server";
-import querystring from "querystring";
+import app from "../server.js";
 
-export default function handler(req: any, res: any) {
-  // Reconstruct the URL for Express
-  const pathParam = req.query?.path;
-  if (pathParam) {
-    const query = { ...req.query };
-    delete query.path;
-    const qs = querystring.stringify(query);
-    req.url = '/api/' + pathParam + (qs ? '?' + qs : '');
-  }
-  
-  // Pass the Vercel request directly into our Express app
-  return app(req, res);
-}
+export default app;
